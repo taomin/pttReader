c6d89421c3ebceb460cad3a7c809534baddbab8f
itia chang <itiachang@gmail.com>
2012-05-01 01:05:50 -0700

    fix code indention

--- public/javascripts/fbinit.js
+++ public/javascripts/fbinit.js
@@ -3,26 +3,26 @@
  */
 
 window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '177950445641724', // App ID
-      channelUrl : '//localhost:3000/files/channel.html', // Channel File
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
-    });
+  FB.init({
+    appId      : '177950445641724', // App ID
+    channelUrl : '//localhost:3000/files/channel.html', // Channel File
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
 
-    // Additional initialization code here
+  // Additional initialization code here
 
-	var fbreader = new FBReader(FB);
-	fbreader.init();
+  var fbreader = new FBReader(FB);
+  fbreader.init();
 };
 
 // Load the SDK Asynchronously
 $('.fb-login').click(function(){
-	var d=document, js, id = 'facebook-jssdk', ref = d.getElementsByTagName('head')[0];
- 	if (d.getElementById(id)) {return;}
- 	js = d.createElement('script'); js.id = id; js.async = true;
- 	js.src = "//connect.facebook.net/en_US/all.js";
- 	ref.parentNode.insertBefore(js, ref);
- 	$('.fb-login-caption').css('display','none');
-	});
+  var d=document, js, id = 'facebook-jssdk', ref = d.getElementsByTagName('head')[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement('script'); js.id = id; js.async = true;
+  js.src = "//connect.facebook.net/en_US/all.js";
+  ref.parentNode.insertBefore(js, ref);
+  $('.fb-login-caption').css('display','none');
+});
--- public/javascripts/reader.js
+++ public/javascripts/reader.js
@@ -1,58 +1,58 @@
+/**
+ * Facebook Reader : interact with FB api, fetch your timeline updates from facebook 
+ * @param Object FB : facebook javascript library instance
+ */
 function FBReader (FB) {
-	this.FB = FB;
-
-	//this.accessToken = null;
-	//this.userID = null;
+  this.FB = FB;
 };
 
 (function(){
 
-	/**
-	 * Private property declaration
-	 */
-	var _accessToken = null,
-		_userID = null,
-		_userName = null;
+  /**
+   * Private property declaration
+   */
+  var _accessToken = null,
+    _userID = null,
+    _userName = null;
 
-	/**
-	 * FB access initialization 
-	 * @return null
-	 */
-	FBReader.prototype.init = function () {
-		FB.login(function(response) {
-			if (response.authResponse) {
-				_accessToken = response.authResponse.accessToken;
-				_userID = response.authResponse.userID;
-		    	console.log('Welcome!  Fetching your information.... ');
-		    	
-		     	FB.api('/me', function(response) {
-		       		console.log('Good to see you, ' + response.name + '.');
-		       		_userName = response.name;
-
-		    	});
+  /**
+   * FB access initialization 
+   * @return null
+   */
+  FBReader.prototype.init = function () {
+    FB.login(function(response) {
+      if (response.authResponse) {
+        _accessToken = response.authResponse.accessToken;
+        _userID = response.authResponse.userID;
+          console.log('Welcome!  Now we are able to fetch your information.... ');
+          
+          // FB.api('/me', function(response) {
+          //   console.log('Good to see you, ' + response.name + '.');
+          //   _userName = response.name;
+          // });
                 
-		    	FB.api('/me/home', function(response){
-                    //do something
-		    	});
-		   	} else {
-		     	console.log('User cancelled login or did not fully authorize.');
-		   	}
-		},{scope: 'user_activities,user_interests,user_likes,user_status,friends_activities,friends_status,publish_actions,read_stream,offline_access'});
-	};
+          // FB.api('/me/home', function(response){
+          //   //do something
+          // });
+        } else {
+          console.log('User cancelled login or did not fully authorize.');
+        }
+    },{scope: 'user_activities,user_interests,user_likes,user_status,friends_activities,friends_status,publish_actions,read_stream,offline_access'});
+  };
 })();
 
 
-	//start doing other things...
-  	/*
-  	 * get ptt data from yql
+  //start doing other things...
+    /*
+     * get ptt data from yql
 
 
     var api_key = '',
         yqlurl = 'http://query.yahooapis.com/v1/public/yql?q=use%20%22store%3A%2F%2FFmNpgmPDX9VzBzjdurhPfs%22%20as%20ptt%3B%20select%20*%20from%20ptt%20where%20api_key%3D"' + api_key + '"%20and%20category%3D%220%22%20&jsonCompat=new';
 
-	$.get(yqlurl, function(data){
+  $.get(yqlurl, function(data){
 
-		console.log('data received', data);
-	});
+    console.log('data received', data);
+  });
 
-	*/
+  */
